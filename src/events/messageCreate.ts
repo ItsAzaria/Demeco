98f12ea872c6dbb0b821568f0f234122820a6709
moe <moe9999@protonmail.com>
2020-06-03 23:13:15 +0100

    :sparkles: Now works on multiple temperatures per message

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -5,25 +5,36 @@ import { ftoc, ctof } from '../utils/conversionUtils.ts';
 export const messageCreate = (message: Message) => {
     if (message.author.bot) return;
 
-    const regex = new RegExp(/(-?\d+)\s?(c|f)/i);
-    const match = message.content.match(regex);
-    if(!match) return;
+    const regex = new RegExp(/(-?\d+)\s?(c|f)/ig);
+    const matchAll = message.content.matchAll(regex);
+    if(!matchAll) return;
 
-    const value = parseFloat(match[1]);
-    let result = null;
+    let msg = '';
 
-    // yes a switch, deal with it
-    switch(match[2].toLowerCase()) {
-        case 'f':
-            result = ftoc(value)
-            break;
-        case 'c':
-            result = ctof(value);
-            break;
+    for(let match of matchAll) {
+        const value = parseFloat(match[1]);
+        let result = null;
+        let original = null;
+    
+        // yes a switch, deal with it
+        switch(match[2].toLowerCase()) {
+            case 'f':
+                original = value + '°F'
+                result = ftoc(value);
+                break;
+            case 'c':
+                original = value + '°C'
+                result = ctof(value);
+                break;
+        }
+    
+        if (result) {
+            msg += `${original} == ${result}\n`;
+        }
     }
 
-    if (result) {
-        sendMessage(message.channel, `${match[0]} otherwise known as ${result}`);
-    }
+    sendMessage(message.channel, msg);
+
+    
     
 };
